144bf115f008948556c48184833c723a70b4d6e8
dennisdebel <dennisdebel@gmail.com>
2014-08-04 17:57:29 +0200

    single page css

--- single.php
+++ single.php
@@ -9,32 +9,31 @@
 	</div>
 
 	
-	<div id="container">
-	
+	<div class="single_post_container">
+		
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+		<center><div class="post_thumbnail">
+		<?php if (has_post_thumbnail( $post->ID ) ): ?> 
+			<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 
-<?php if (has_post_thumbnail( $post->ID ) ): ?>
-					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+			<img id="caption_image" src="<?php echo $image[0]; ?>">
+			<?php endif; ?>
 
-					<img id="largeImage" src="<?php echo $image[0]; ?>">
-					<?php endif; ?>
+		</div></center>
 
-
-
-				<div class="entry">
-					
-					<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-					
-					<?php the_tags( 'Tags: ', ', ', ''); ?>
-				</div>
-				
-				
+		<div class="entry">
+			<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
 			
-			<?php $content = get_the_content(); echo $content;?>
-
-
-		<!--<?php the_content(); ?>-->
+			<?php the_tags( 'Tags: ', ', ', ''); ?>
+		</div>
+				
+		<div class="post_content"			
+			<?php $content = get_the_content();
+				$content = apply_filters('the_content', $content);
+				echo wpautop($content);?>
+			<!--<?php the_content(); ?>-->
+		</div>
 		<br><br>
 		
 		<?php edit_post_link('Edit this entry','','.'); ?>
